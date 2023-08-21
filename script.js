92082415554b0f3cc3da5700a5f98c850eff9ffd
BenGavieres <benjamin.gavieres@gmail.com>
2023-08-21 15:07:04 -0400

    Create displayController module. Add event listeners to all field elements.

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ const Player =(symbol)=>{
         return symbol;
     }
 
-    return {
-        getSymbol
-    };
+    const getSign = () => {
+        return sign;
+      };
+
+    return {getSymbol};
 }
+
+let playerOne=Player("X");
 
 //Gameboard module; created immediately due to module pattern
 const board=(()=>{
@@ -25,9 +29,26 @@ const board=(()=>{
         return squares[i];
     }
 
-    const clear()=>{
+    const clear=()=>{
         for(let i=0;i<squares.length;i++){
             squares[i]="";
         }
     }
+
+    return {
+        setField,
+        getField,
+        clear
+    }
+})();
+
+//Display controller module
+const displayController=(()=>{
+    fields=document.querySelectorAll(".field");
+
+    fields.forEach((field)=>{
+        field.addEventListener("click",e=>{
+            field.textContent=playerOne.getSymbol();
+        })
+    })
 })();
